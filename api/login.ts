a2a6337eb2e05c94fdba423abb3a376def62d69a
China-quanda <864910436@qq.com>
2024-07-31 09:14:47 +0800

    login

--- api/login.ts
+++ api/login.ts
@@ -16,34 +16,80 @@ export interface PhoneCodeLoginData {
 /**
  * 快捷登录
  */
-export const phoneNumberLogin =(data:PhoneNumberLoginData):Promise<PhoneNumberLoginRes>=> {
-	console.log('phoneNumberLogin',data);
+export const phoneNumberLogin = (data: PhoneNumberLoginData): Promise<PhoneNumberLoginRes> => {
+  console.log('phoneNumberLogin', data);
   return request({
     url: '/login/phoneNumberLogin',
     method: 'POST',
-		data
+    data
   })
 }
 /**
  * 手机号码登录
  */
-export const phoneCodeLogin =(data:PhoneCodeLoginData):Promise<PhoneNumberLoginRes>=> {
+export const phoneCodeLogin = (data: PhoneCodeLoginData): Promise<PhoneNumberLoginRes> => {
   return request({
     url: '/login/phoneCodeLogin',
     method: 'POST',
-		data
+    data
   })
 }
 
 /**
  * 获取验证码
  */
-export const getSmsCode =(phone:string):Promise<string>=> {
+export const getSmsCode = (phone: string): Promise<string> => {
   return request({
     url: '/login/phoneCodeLogin',
     method: 'GET',
-		params:{
-			phone
-		}
+    params: {
+      phone
+    }
   })
+}
+
+/**
+ * 获取code
+ */
+export const getCode = (): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    uni.login({
+      provider: 'weixin',
+      success: (result) => {
+        if (result && result.code) {
+          resolve(result.code)
+        } else {
+          reject(result)
+        }
+      },
+      fail: (error) => {
+        reject(error)
+        uni.showToast({
+          title: error.errMsg || '获取code失败！',
+          icon: 'none'
+        })
+      }
+    })
+  })
+}
+
+export const weixinLogin = (code: string) => {
+  return request({
+    url: '/login/miniapp/weixinLogin',
+    method: 'POST',
+    params: {
+      code
+    }
+  })
+}
+
+/**
+ * 登录
+ * @returns 
+ */
+export const login = async () => {
+  const code = await getCode()
+  const result = await weixinLogin(code);
+  console.log('result', result);
+  return result
 }
